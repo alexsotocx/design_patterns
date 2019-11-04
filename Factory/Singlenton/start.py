7f4209503c25928beb7613060292e34b0283638f
Alexander Soto <alexsotocx@gmail.com>
2019-11-04 06:19:53 +0100

    Singleton and include notes

--- Factory/Singlenton/start.py
+++ Factory/Singlenton/start.py
@@ -1,4 +1,10 @@
 from db_connection import DatabaseConnection
 
 connection = DatabaseConnection.create()
-connection.create()
+connection.connect()
+
+other_method_connection = DatabaseConnection.create()
+
+if other_method_connection == connection:
+    print("They are the same")
+
